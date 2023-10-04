1546273df9bf0340282100ca65890dcc4e05383d
Shafiq Ahmed Khan <shafiqahmedkhan@shafiqs-mbp.home>
2023-10-04 10:05:01 +0100

    configured selenium

--- src/test/java/stepDefinitions/MyStepdefs.java
+++ src/test/java/stepDefinitions/MyStepdefs.java
@@ -3,11 +3,16 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyStepdefs {
     @Given("I have an AUT")
     public void iHaveAnAUT() {
         System.out.println("1");
+        System.setProperty("webdriver.chrome.driver", "/Users/shafiqahmedkhan/Downloads/chromedriver-mac-arm64/chromedriver");
+        WebDriver driver= new ChromeDriver();
+        driver.get("https://utilita.co.uk/");
     }
 
     @When("I test this AUT")
